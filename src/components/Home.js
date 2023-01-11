9508a355d7500b2a4b8bd2dbe30168f4de461e12
Maryann Ruguru Njuguna <maryannruguru20@gmail.com>
2023-01-11 21:29:46 +0300

    completed assignment

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,18 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+ // return <div id="#home">Home
+  return (
+  <div id="home">
+  <h1 style={{color: "firebrick"}}>{name} is a Web Developer from {city} </h1>
+  </div>)
 }
 
 export default Home;
+// an <h1> element, with the style attribute set to a color of "firebrick", and the content of "Your Name is a Web Developer from Your City"?
+//how to add a h1 element with a style attribute inside a jsx in react?
+
+
+
+
+
